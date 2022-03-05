8846d3aecfa5d1e83500d4966c05d6e49107a157
DTayloe <diomoidapple@gmail.com>
2022-03-05 10:14:06 -0500

    Feature: Load previous file

--- TestWFA/TaskController.cs
+++ TestWFA/TaskController.cs
@@ -56,7 +56,16 @@ namespace TestWFA
                _model.SetController(this);
                _view.SetController(this);
 
-               LoadModelToView();
+               string startFilePath = _model.ReadStartFilePathFromRegistry();
+
+               if (startFilePath != null)
+               {
+                    LoadXmlFileToModel(startFilePath);
+               }
+               else
+               {
+                    LoadModelToView();
+               }
           }
           
           /// <summary>
@@ -409,13 +418,13 @@ namespace TestWFA
 
                     xr.Close();
                     Console.WriteLine();
-
-                    LoadModelToView();
-
-                    UnsavedChanges = false;
-
-                    ErrorScan();
                }
+
+               LoadModelToView();
+
+               UnsavedChanges = false;
+
+               ErrorScan();
           }
 
           public List<TaskItem> GetRunningTasks()
